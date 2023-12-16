582b3b11f56e7f92c6d5f209df3c00cff998f182
Al Muhdil Karim <119833237+almuhdilkarim13@users.noreply.github.com>
2023-12-17 00:06:37 +0700

    Update itdev-rhel-desktop-user.sh

--- itdev-rhel-desktop-user.sh
+++ itdev-rhel-desktop-user.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-user_interface() {
+layout_config() {
+
 	gsettings set org.gnome.desktop.interface gtk-theme Orchis-Dark-Compact;
 	gsettings set org.gnome.desktop.interface icon-theme WhiteSur-dark;
 	gsettings set org.gnome.desktop.interface text-scaling-factor 0.99;
-	gsettings set org.gnome.desktop.wm.preferences button-layout "right:close,minimize,maximize"
+	gsettings set org.gnome.desktop.wm.preferences button-layout "right:minimize,maximize,close"
 	gsettings set org.gnome.desktop.interface clock-show-seconds true;
 	gsettings set org.gnome.desktop.interface clock-show-weekday true;
 	gsettings set org.gnome.desktop.calendar show-weekdate true;
+}
 
-	if [ -d ~/.ssh ];
+
+sshd_configs() {
+
+	if [ -d $HOME/.ssh ] 
 	then
 		echo "directory exist";
 	else
@@ -19,4 +24,10 @@ user_interface() {
 }
 
 
-user_interface;
+base_config() {
+	layout_config &&
+	sshd_configs;  
+}
+
+
+base_config;
